6c829c3ec94fe31c8514e370ba99d83e32f1f696
Aglina Lika <62331304+Aglina-Lika@users.noreply.github.com>
2025-12-29 13:14:03 +0100

    Update simFunc_a_D1D2_D.R

--- Area/Simulation+Fit/simFunc_a_D1D2_D.R
+++ Area/Simulation+Fit/simFunc_a_D1D2_D.R
@@ -111,7 +111,7 @@ for (i in 191:200){
   
   # fit the STAN model 
   
-  fit <- stan(file = "V://Users//055609(A_Lika)//new codes 2 stat paper//linear time _ separated//area (time +sex)//funcForm_area.stan", 
+  fit <- stan(file = "Area//STAN codes//funcForm_area_D.stan", 
               data = Data, chains = 4, iter = 2000, thin = 1, 
               control = list(max_treedepth = 20, adapt_delta = 0.99))
   # save the time of the STAN model of the i-th simulation  
@@ -121,8 +121,9 @@ for (i in 191:200){
                                    pars = c("temp_alphaA","temp_betas1","temp_betas2","sigma1","sigma2","D"))$summary)
   
 
-  save(summary1, file = paste0("summary_", i, ".RData"))  
+  save(summary1, file = paste0("Area//A_D1D2_D//summary_", i, ".RData"))  
   
 
 }
+
 
