a6c78ab5675b76d7f6caf8fb2d448fddb4986644
Aglina Lika <62331304+Aglina-Lika@users.noreply.github.com>
2025-12-29 13:03:24 +0100

    Update simFunc_v&a_D1D2_D1D2.R

--- Area and Value/Simulation+Fit/simFunc_v&a_D1D2_D1D2.R
+++ Area and Value/Simulation+Fit/simFunc_v&a_D1D2_D1D2.R
@@ -118,7 +118,7 @@ for (i in 1:200){
   
   # fit the STAN model 
   
-  fit <- stan(file = "V://Users//055609(A_Lika)//new codes 2 stat paper//linear time _ separated//value+area (time+sex)//funcForm_v&area_D1D2 .stan", 
+  fit <- stan(file = "V://Users//055609(A_Lika)//new codes 2 stat paper//linear time _ separated//value+area (time+sex)//funcForm_v&a_D1D2 .stan", 
               data = Data, chains = 4, iter = 2000, thin = 1, 
               control = list(max_treedepth = 20, adapt_delta = 0.99))
   # save the time of the STAN model of the i-th simulation  
@@ -128,12 +128,8 @@ for (i in 1:200){
                                    pars = c("temp_alphaV","temp_alphaA","temp_betas1","temp_betas2","sigma1","sigma2","D_1","D_2"))$summary)
   
   
-  save(summary1, file = paste0("summary_", i, ".RData"))
+  save(summary1, file = paste0("Area and Value//V_A_D1D2_D1D2//summary_", i, ".RData"))
 }
-
-
-#time_ela1<-time_ela[!is.na(time_ela)]
-#write(time_ela1,"V://Users//055609(A_Lika)//new codes 2 stat paper//linear time _ separated//value+area (time+sex)//simulation results_D1D2_D1D2//time_3_10.text")
 
 
 
